9adc5253a72263fb1a9eef605198fe75873d759b
ouhoy <kimkinon@gmail.com>
2023-09-25 19:09:18 +0100

    Add starting files

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const {post} = defineProps<{ post: { title: string, tags: string[] } }>()
+
+</script>
+
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+    <p>{{  }}</p>
+    <span v-for="tag in post.tags" :key="tag">
+      #{{ tag }}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+import PostList from '../components/PostList.vue'
+
+
+
+
+
+</script>
+
+
+<template>
+  <div class="home">
+<h1>Hello from Home!!</h1>
+  </div>
+</template>
+
+
+
+
+
